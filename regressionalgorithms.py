3352039fb579e7428264a5900b5a0f72a0185c38
James <rwu4@ualberta.ca>
2017-12-04 12:19:36 -0700

    Only keeping the code I wrote

--- regressionalgorithms.py
+++ regressionalgorithms.py
@@ -50,70 +50,7 @@ class Regressor:
         return np.linalg.norm(np.subtract(prediction,ytest))
 
 
-class RangePredictor(Regressor):
-    """
-    Random predictor randomly selects value between max and min in training set.
-    """
 
-    def __init__( self, parameters={} ):
-        """ Params can contain any useful parameters for the algorithm """
-        self.params = {}
-        self.reset(parameters)
-
-    def reset(self, parameters):
-        self.resetparams(parameters)
-        self.min = 0
-        self.max = 1
-        
-    def learn(self, Xtrain, ytrain):
-        """ Learns using the traindata """
-        self.min = np.amin(ytrain)
-        self.max = np.amax(ytrain)
-
-    def predict(self, Xtest):
-        ytest = np.random.rand(Xtest.shape[0])*(self.max-self.min) + self.min
-        return ytest
-
-class MeanPredictor(Regressor):
-    """
-    Returns the average target value observed; a reasonable baseline
-    """
-    def __init__( self, parameters={} ):
-        self.params = {}
-        self.reset(parameters)
-
-    def reset(self, parameters):
-        self.resetparams(parameters)
-        self.mean = None
-        
-    def learn(self, Xtrain, ytrain):
-        """ Learns using the traindata """
-        self.mean = np.mean(ytrain)
-
-    def predict(self, Xtest):
-        return np.ones((Xtest.shape[0],))*self.mean
-
-
-class FSLinearRegression(Regressor):
-    """
-    Linear Regression with feature selection, and ridge regularization
-    """
-    def __init__( self, parameters={} ):
-        self.params = {'features': [1,2,3,4,5]}
-        self.reset(parameters)
-
-    def learn(self, Xtrain, ytrain):
-        """ Learns using the traindata """
-        # Dividing by numsamples before adding ridge regularization
-        # to make the regularization parameter not dependent on numsamples
-        numsamples = Xtrain.shape[0]
-        Xless = Xtrain[:,self.params['features']]
-        self.weights = np.dot(np.dot(np.linalg.inv(np.dot(Xless.T,Xless)/numsamples), Xless.T),ytrain)/numsamples
-
-    def predict(self, Xtest):
-        Xless = Xtest[:,self.params['features']]
-        ytest = np.dot(Xless, self.weights)
-        return ytest
 
 class RidgeLinearRegression(Regressor):
     """
